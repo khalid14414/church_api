d7cb3a858a7678c30228d4b71ab45a2bbb0d5bbb
khalid14414 <qwekukhalid266@gmail.com>
2025-05-12 22:39:09 +0000

    error fixed

--- config/passport.js
+++ config/passport.js
@@ -32,7 +32,9 @@ passport.use(new GoogleStrategy({
                 name:profile.displayName,
                 email:profile.emails[0].value,
                 googleId:profile.id,
-                avatar:profile.photos[0].value
+                avatar:profile.photos[0].value,
+                isEmailVerified: profile.emails[0].verified,
+                
          })
          return done(null,newUser)
     }
